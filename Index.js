ef54284b828f45831e9876a74f36c1df38e0b716
NakraniKrupali0 <92012510+NakraniKrupali@users.noreply.github.com>
2021-10-08 20:45:18 +0000

    zf

--- Index.js
+++ Index.js
@@ -75,7 +75,7 @@ var create_Dir = () => {
 };
 
 var removeDirWizard = () => {
-  rl.question("Enter Directory Name :- ",(ans) => {
+  rl.question("Enter Directory Name : ",(ans) => {
     dirname = ans;
     removeDir();
   })
@@ -89,6 +89,117 @@ var removeDir = () => {
         console.log(dirname + " Directory Removed Successfully...");
       }
       repeat();
+   });
+};
+
+var writeFileWizard = () => {
+  rl.question("Enter File Name : ",(ans) => {
+      filename = ans;
+      rl.question("Enter File Content : ",(ans) => {
+         content = ans;
+         writeFileData();
+      });
+  });
+};
+
+var writeFileData = () => {
+   fs.writeFile(filename + ".txt",content,(err) => {
+      if(err){
+        console.log(err);
+      }
+      else
+      {
+        console.log("File Created SuccessFully...!",filename);
+      }
+      repeat();
+   });
+};
+
+var readFileWizard = () => {
+   rl.question("Enter File Name : ",(ans) => {
+      filename = ans;
+      fs.readFile(filename + ".txt","utf8",(err,result) => {
+         if(err){
+            console.log(err);
+         }else{
+            console.log(result);
+         }
+         repeat();
+      });
+   });
+};
+
+var deleteFileWizard = () => {
+   rl.question("Enter File Name : ",(ans) => {
+     fs.unlink(ans + ".txt",(err) => {
+         if(err){
+              console.log(err);
+         }else{
+              console.log(ans + " File Deleted SuccessFully ....");
+         }
+         repeat();
+     });
+   });
+};
+
+var appendFileWizard = () => {
+  rl.question("Enter File Name To A Append : ",(ans) => {
+     filename = ans;
+     rl.question("Enter Content :",(ans) => {
+        content = ans;
+        fs.appendFile(filename + ".txt",content,(err) => {
+          if(err){
+             console.log(err);
+          }else{
+             console.log(filename +" File Appended Successfully..");
+          }
+          repeat();
+        });
+     });
+  });
+};
+
+var replaceFileWizard = () => {
+    rl.question("Enter Your File Name : ",(ans) => {
+       filename = ans;
+       rl.question("Enter Content Of Replace : ",(ans) => {
+         content = ans;
+         rl.question("Enter New Content To Replace : ",(ans) => {
+           const replace_Str = ans;
+           fs.readFile(filename + ".txt","utf8",(err,data) => {
+              if(err){
+                 console.log(err);
+                 repeat();
+              }else{
+                const res = data.replace(content,replace_Str);
+                fs.writeFile(filename + ".txt",res,(err) => {
+                  if(err){
+                      console.log(err);
+                  }else{
+                     console.log(filename + " File Updated / Replaced SuccessFully...");
+                  }
+                  repeat();
+                });
+              }
+           });
+         });
+       });
+    });
+};
+
+var renameFileWizard = () => {
+   rl.question("Enter Old File Name : ",(ans) => {
+     var oldFile = ans;
+     rl.question("Enter New File Name : ",(ans) => {
+       fs.rename(oldFile + ".txt",ans + ".txt",(err) => {
+          if(err){
+             console.log(err);
+          }else{
+             console.log(ans + ".txt File Renamed SuccessFully ...");
+          }
+          repeat();
+       }); 
+     });
    });
 };
 
